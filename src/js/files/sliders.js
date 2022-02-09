cd883e7ed6f1e83d79a7c231de1f82f55ac74316
MaksPronevich <56038937+MaksPronevich@users.noreply.github.com>
2022-02-09 16:58:03 +0300

    update

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -99,6 +99,74 @@ function initSliders() {
 			on: {},
 		});
 	}
+
+	if (document.querySelector('.doctors__slider')) {
+		new Swiper('.doctors__slider', {
+			modules: [Navigation],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 2,
+			spaceBetween: 24,
+			// autoHeight: false,
+			// speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагинация
+			/*
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			*/
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.doctors-slider-arrow-prev',
+				nextEl: '.doctors-slider-arrow-next',
+			},
+
+			// Брейкпоинты
+
+			breakpoints: {
+				0: {
+					slidesPerView: 1.1,
+					spaceBetween: 10,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 25,
+				},
+				992: {
+					// spaceBetween: 25,
+				},
+			},
+
+			// События
+			on: {},
+		});
+	}
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
