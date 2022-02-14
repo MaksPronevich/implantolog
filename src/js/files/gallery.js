92db370fc73f54eb5083f75bf550dd6d8ce7fb37
MaksPronevich <56038937+MaksPronevich@users.noreply.github.com>
2022-02-14 14:12:55 +0300

    update

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -1,4 +1,3 @@
-
 /*
 Документация по работе в шаблоне: https://www.lightgalleryjs.com/docs/
 Документация плагина: https://www.lightgalleryjs.com/docs/
@@ -6,25 +5,25 @@
 */
 
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile, FLS } from "./functions.js";
+import { isMobile, FLS } from './functions.js';
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js';
 
 // Подключение базового набора функционала
 import lightGallery from 'lightgallery';
 
 // Плагины
 // lgZoom, lgAutoplay, lgComment, lgFullscreen, lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo, lgMediumZoom
-// import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.min.js'
-//import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
+import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.min.js';
+import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js';
 
 // Базовые стили
 import '@scss/libs/gallery/lightgallery.scss';
 // Стили дополнений
-// import '@scss/libs/gallery/lg-thumbnail.scss';
+import '@scss/libs/gallery/lg-thumbnail.scss';
 // import '@scss/libs/gallery/lg-video.scss';
 // import '@scss/libs/gallery/lg-autoplay.scss';
-// import '@scss/libs/gallery/lg-zoom.scss';
+import '@scss/libs/gallery/lg-zoom.scss';
 // import '@scss/libs/gallery/lg-pager.scss';
 // import '@scss/libs/gallery/lg-fullscreen.scss';
 // import '@scss/libs/gallery/lg-share.scss';
@@ -44,17 +43,12 @@ if (galleries.length) {
 		galleyItems.push({
 			gallery,
 			galleryClass: lightGallery(gallery, {
-				// plugins: [lgZoom, lgThumbnail],
+				plugins: [lgZoom, lgThumbnail],
 				licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
 				speed: 500,
-			})
-		})
+			}),
+		});
 	});
 	// Добавляем в объект модулей
 	flsModules.gallery = galleyItems;
 }
-
-
-
-
-
